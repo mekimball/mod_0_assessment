ba52550f1852e381afbe3ad52a570280ffc1d092
Matthew Kimball <mekimba6@gmail.com>
2021-06-11 19:23:38 -0600

    Add dinosaur hash

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+dinosaur = {
+  species: "Tyranosaurs Rex",
+  meaning: "Tyrant Lizard King",
+  weight: 9.3,   # short tons
+  height: 12,   # feet
+  is_cool: true,
+  is_scary: true,
+  scary_parts: ["teeth", "claws" "big feets"],
+  starred_in: ["Jurrasic Park", "Fantasia", "Jurassic Park 3"]
+}
