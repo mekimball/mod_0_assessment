f4a0106a69c6a06b87ee72b02e9e72d7f83e5f10
Matthew Kimball <mekimba6@gmail.com>
2021-06-11 19:30:31 -0600

    Add developer hash

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+developer = {
+  first_name: "Jim",
+  last_name: "Halpert",
+  age: 37,  #years
+  weight: 167.5, #lbs
+  loves_pam: true,
+  hates_toby: true,
+  friends: ["Dwight", "Michael" "Kevin"],
+  interests: ["pranks", "Philadelphia Eagles", "Eminem"]  # I don't actually watch the Office
+}
