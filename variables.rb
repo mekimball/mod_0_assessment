28f6dada4b25c4301e4b20332539f6115f268264
Matthew Kimball <mekimba6@gmail.com>
2021-06-11 19:13:55 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,9 @@
+movie = "Die Hard"
+is_entertaining = true
+movie = "Die Hard 2"
+num_4 = 4
+num_6 = 6
+product = num_4 * num_6
+favorite_foods = ["eggs", "pizza", "watermelon"]
+favorite_foods.pop
+p movie.length
